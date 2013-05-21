978bfd62d30482f57649ad5409fb56ae31dfc170
Nuno Miguel Levezinho da Silva <gamesflash94@gmail.com>
2013-05-21 16:11:02 +0100

    Updated main.c with headers and cleaner code

--- Main.c
+++ Main.c
@@ -2,37 +2,21 @@
 #include<string.h>
 #include<stdlib.h>
 #include "Displays.h"
+#include "FileFunctions.h"
+#include "StructFunctions.h"
 
 #define RETAIL_FILE_NAME "retail.bin"
-#define _CRT_SECURE_NO_WARNINGS 1
-#define BSIZE 256
-
-typedef struct produto produto, *prod_pointer;
-	struct produto
-	{
-		int IDProduto;
-		int quantidade;
-		int coordenadas[1][1]; //Corredor + Armario
-		prod_pointer proximo;
-	};
-
-
-void dump_buffer(void *buffer, int buffer_size)
-{
-  int i;
-  for(i = 0;i < buffer_size;++i)
-     printf("%c\n", ((char *)buffer)[i]);
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
 
 int main()
 {
-
+	/*Declaração de Variáveis*/
+	prod_pointer Produto = NULL;
 	FILE *file;
 	char *buffer;
-	int FileLenght, n_corredores, n_armarios, resp_menu=0, resp_sub_menu=0;
-	int i, j, k;
+	int FileLenght, NCorredores, NArmarios, resp_menu=0, resp_sub_menu=0, i, j, k, aux;
 
-	//Abre o ficheiro
+	/*Abre o ficheiro*/
 	file = fopen(RETAIL_FILE_NAME, "rb");
 	if (!file)
 	{
@@ -48,22 +32,26 @@ int main()
 
 	if(!buffer)
 	{
-		return;
+		return 1;
+	}
+	
+	fread(&NArmarios, sizeof(int), 1, file);   //Obtem numero de armarios
+	fread(&NCorredores, sizeof(int), 1, file); //Obtem numero de corredores
+	NArmarios+=1;
+	NCorredores+=1;
+	//printf("IMP: %d e %d\n", NArmarios, NCorredores);
+
+	fread(&k, sizeof(int), 1, file); //nº produtos no 1º armario --DECIDIR COMO RESOLVER ISTO
+
+	for (i = 1; i < NCorredores; i++) {
+		for (j = 1; j < NArmarios; j++) {
+			printf("Corredor: %d\nArmario: %d\n", i, j);
+			//Guarda dados na struct
+			Produto=AdicionaProduto(Produto, GetNextData(file), GetNextData(file), i, j);
+		}
 	}
 
-
-	i = fread(buffer, FileLenght, 1, file);
-	printf("Elementos Lidos: %d\n", i);
-	//Lê o conteúdo do ficheiro
-	//while(!feof(file)){
-		//fgets(buffer, FileLenght, file);
-		//printf("Data[]: %s\n", buffer);   
-	//}
-
-	dump_buffer(buffer, FileLenght);
-
 	fclose(file);
-
 
 	/*			Corredor 1
 		3P 2unidades 4 4unidades 7 78unidades 1	
@@ -92,219 +80,85 @@ int main()
 
 	*/
 
-	//DisplayMenu();
-	scanf("%d", &resp_menu);
+		DisplayMenu();
+		scanf("%d", &resp_menu);
 
-	switch(resp_menu)
-	{
-	case 1:
+		switch(resp_menu)
 		{
-			/*     -- Visualização / Pesquisa --
-				O programa deve poder fazer listagens completas e parciais dos produtos existentes em
-				armazém. Exemplos de listagens parciais incluem: mostrar todos os produtos de um
-				determinado corredor/armário ou indicar a localização/quantidade em stock de um
-				produto específico. De acordo com a preferência do utilizador, a informação pode ser
-				apresentada na consola ou enviada para um ficheiro de texto. 
-			*/
-			system("CLS");
-			DisplayMenuVisualizacoes();
-			scanf("%d", &resp_sub_menu);
-			switch(resp_sub_menu)
+		case 1:
 			{
-			case 1:
+				/*     -- Visualização / Pesquisa --
+					O programa deve poder fazer listagens completas e parciais dos Produtos existentes em
+					armazém. Exemplos de listagens parciais incluem: mostrar todos os Produtos de um
+					determinado corredor/armário ou indicar a localização/quantidade em stock de um
+					Produto específico. De acordo com a preferência do utilizador, a informação pode ser
+					apresentada na consola ou enviada para um ficheiro de texto. 
+				*/
+				system("CLS");
+				DisplayMenuVisualizacoes();
+				scanf("%d", &resp_sub_menu);
+				switch(resp_sub_menu)
 				{
-					printf("Vale das Flores Retail\n\n");
-					printf("\t\tCorredor 1\n");
-					printf("\t\tArmario 1\n");
-					printf("\t2 Unidades do produto 4\n");
-					printf("\t4 Unidades do produto 7\n");
-					printf("\t78 Unidades do produto 1\n");
-					system("pause");
-				}
-			case 2:
-				{
-					system("CLS");
-				}
-			case 3:
-				{
-					system("CLS");
-				}
-			case 4:
-				{
-					break;
-				}
-			default:
-				{
-					printf("Opcao Invalida!\n");
-					break;
-				}
-			}			
-			break;
+				case 1:
+					{
+						MostraProdutos(Produto);
+						system("pause");
+						break;
+					}
+				case 2:
+					{
+						EliminaProd(Produto);
+						system("CLS");
+						break;
+					}
+				case 3:
+					{
+						system("CLS");
+						break;
+					}
+				case 4:
+					{
+						break;
+					}
+				default:
+					{
+						printf("Opcao Invalida!\n");
+						break;
+					}
+				}			
+				break;
+			}
+		case 2:
+			{
+				/*		 -- Gestao de Encomendas --
+					Ao receber uma encomenda, o programa deve verificar se existem Produtos em stock
+					para satisfazer a totalidade da encomenda. Caso falte algum Produto, a encomenda é
+					cancelada e gerado um alerta sobre o Produto (ou Produtos) em falta.
+				*/
+				break;
+			}
+		case 3:
+			{
+				/*		-- Reposição de Stocks --
+					Deve ser possível actualizar as quantidades de Produtos existentes em armazém e
+					adicionar novos Produtos.
+					Nesta funcionalidade, o programa deve aceder ao ficheiro de texto e actualizar as
+					quantidades em stock. Se um Produto surgir no ficheiro, mas não existir em armazém,
+					deve ser colocado num dos armários com menos diversidade de Produtos.
+				*/
+				break;
+			}
+		case 4:
+			{
+				break;
+				return EXIT_SUCCESS;
+			}
+		default:
+			{
+				printf("Opcao Invalida! Programa vai Terminar!\n");
+				return EXIT_FAILURE;
+			}
 		}
-	case 2:
-		{
-			/*		 -- Gestao de Encomendas --
-				Ao receber uma encomenda, o programa deve verificar se existem produtos em stock
-				para satisfazer a totalidade da encomenda. Caso falte algum produto, a encomenda é
-				cancelada e gerado um alerta sobre o produto (ou produtos) em falta.
-			*/
-			break;
-		}
-	case 3:
-		{
-			/*		-- Reposição de Stocks --
-				Deve ser possível actualizar as quantidades de produtos existentes em armazém e
-				adicionar novos produtos.
-				Nesta funcionalidade, o programa deve aceder ao ficheiro de texto e actualizar as
-				quantidades em stock. Se um produto surgir no ficheiro, mas não existir em armazém,
-				deve ser colocado num dos armários com menos diversidade de produtos.
-			*/
-			break;
-		}
-	case 4:
-		{
-			//Programa termina normalmente
-			return EXIT_SUCCESS;
-		}
-	default:
-		{
-			//Nenhuma opção valida, programa termina
-			return EXIT_FAILURE;
-		}
-	}
-
-	/* Programa nao deve chegar aqui #TESTE# */
+	//Programa nao deve chegar aqui #TESTE#
 	printf("\n#TESTE#\n");
 }
-
-/* #define _CRT_SECURE_NO_WARNINGS 1
-
-	
-
-void preenche(pno p)
-{
-	printf("Nome: ");
-	gets(p->nome);
-	printf("Urgencia (1-3): ");
-	scanf("%d", p->urgencia);
-	p->prox = NULL;
-}
-
-int verifica_lista(pno p)
-{
-	if (p == NULL)
-		return 1;
-	else
-		return 0;
-}
-
-pno adiciona_final(pno p)
-{
-	pno novo, aux;
-
-	novo = malloc(sizeof(no));
-
-	if(novo == NULL)
-	{
-		printf("Erro na alocacao de memoria!\n");
-		return p;
-	}
-
-	preenche(novo);
-
-	if(verifica_lista(p))
-	{
-		p = novo;
-	}
-	else
-	{
-		if(novo->urgencia >= 3)
-		{
-			//Restantes Pacientes
-			aux = p;
-			while(aux->prox != NULL)
-				aux = aux->prox;
-			aux->prox = novo;
-		} else if(novo->urgencia == 2)
-		{
-			//Idosos e Criancas
-
-		} else if(novo->urgencia <= 1)
-		{
-			//Gravidas
-			novo->prox = p;
-			p = novo;
-		}
-	}
-	
-	return p;
-}
-
-void mostra_lista(pno p)
-{
-	while(p != NULL)
-	{
-		printf("%s\n",p->nome);
-		p = p->prox;
-	}
-}
-
-pno atendimento(pno p)
-{
-	pno actual;
-	actual = p;
-	p = actual->prox;
-	free(actual);
-	return p;
-}
-
-pno desistencia(pno p)
-{
-
-}
-
-int main()
-{
-	int resp=0;
-	pno lista = NULL;
-	
-	printf("1. Novo Paciente\n");
-	printf("2. Atendimento\n");
-	printf("3. Visualizar Fila de Espera\n");
-	printf("4. Desistencia\n");
-	printf("5. Terminar\n");
-	printf("Escolha uma opcao (1-5): ");
-	scanf("%d", &resp);
-	
-	switch(resp)
-	{
-	case 1:
-		{
-			printf("\nAdicionar Novo Paciente\n\n");
-			lista=adiciona_final(lista);
-		}
-	case 2:
-		{
-			lista=atendimento(lista);
-		}
-	case 3:
-		{
-			mostra_lista(lista);
-		}
-	case 4:
-		{
-			lista=desistencia(lista);
-		}
-	case 5:
-		{
-			printf("Terminado.\n");
-			return 1;
-		}
-	default:
-		{
-			printf("Nenhuma Opcao Selecionada.\n");
-			return 1;
-		}
-	}
-}
-*/
